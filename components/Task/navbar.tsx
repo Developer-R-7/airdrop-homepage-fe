8165edfa0780a96b5ab1c541d857b9abc6c754af
CampVamp <ajayramsaravanan4@gmail.com>
2023-04-17 21:55:40 +0530

    :sparkles: Add Navbar

---
+++ components/Task/navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className=" px-6 py-4 w-full bg-darkteal">
+      <div className=" text-white text-4xl font-semibold">AIRDROP</div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ pages/[slug].tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Task/navbar";
+
+const Task = () => {
+  return (
+    <div className=" w-screen h-screen bg-slategrey">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Task;
